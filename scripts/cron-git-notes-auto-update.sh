429a1488e843781e52fda5d3bcbb96a8082c2a92
Nathaniel Inman <nate@theoestudio.com>
2022-07-08 17:15:37 -0500

    fixed some linting issues with scripts

--- scripts/cron-git-notes-auto-update.sh
+++ scripts/cron-git-notes-auto-update.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-cd /home/nate/Sites/notes
+cd /home/nate/Sites/notes || exit # short-cut if not setup
 
 alias git='/usr/bin/git'
 
 git remote update
 
 LOCAL=$(git rev-parse @) # HEAD
-REMOTE=$(git rev-parse @{u}) # origin/master
-BASE=$(git merge-base @ @{u}) # see where they diverge for rebase
+REMOTE=$(git rev-parse @\{u\}) # origin/master
+BASE=$(git merge-base @ @\{u\}) # see where they diverge for rebase
 
-if [ $LOCAL = $REMOTE ]; then
-  # up-to-date
-elif [ $LOCAL = $BASE ]; then
+if [[ $LOCAL = "$REMOTE" ]]; then
+  echo "up-to-date"
+elif [[ $LOCAL = "$BASE" ]]; then
   git pull --rebase --autostash # local is behind origin/master
-elif [ $REMOTE = $BASE ]; then
+elif [[ $REMOTE = "$BASE" ]]; then
   git push -u origin master # origin/master is behind local
 fi
 
 CHANGES_NUM=$(git status --porcelain | wc -l)
 
-if [ $CHANGES_NUM != 0]; then
+if [[ $CHANGES_NUM != 0 ]]; then
   git add -A
-  git commit -am "cron auto-update `date`"
+  git commit -am "cron auto-update $(date)"
   git push -u origin master
 fi
