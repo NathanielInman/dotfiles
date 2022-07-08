20649dda4387d05edc0dfa68cc17240a190a5f9f
Nathaniel Inman <nate@theoestudio.com>
2022-07-08 16:45:03 -0500

    adjusted cron git auto-update scripts

--- scripts/cron-git-tasks-auto-update.sh
+++ scripts/cron-git-tasks-auto-update.sh
@@ -6,12 +6,23 @@ alias git='/usr/bin/git'
 
 git remote update
 
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse HEAD)
+LOCAL=$(git rev-parse @) # HEAD
+REMOTE=$(git rev-parse @{u}) # origin/master
+BASE=$(git merge-base @ @{u}) # see where they diverge for rebase
 
-if [ $LOCAL != $REMOTE ]; then
-  git pull --rebase --autostash
+if [ $LOCAL = $REMOTE ]; then
+  # up-to-date
+elif [ $LOCAL = $BASE ]; then
+  git pull --rebase --autostash # local is behind origin/master
+elif [ $REMOTE = $BASE ]; then
+  git push -u origin master # origin/master is behind local
+fi
+
+CHANGES_NUM=$(git status --porcelain | wc -l)
+
+if [ $CHANGES_NUM != 0]; then
   git add -A
   git commit -am "cron auto-update `date`"
   git push -u origin master
 fi
+
