3c100c6303f81101fa9b4fdb290d39ad236accc1
Nathaniel Inman <nate@theoestudio.com>
2024-07-08 08:17:33 -0500

    fix: currently playing script handles no album

--- .polybar-scripts/currently-playing.sh
+++ .polybar-scripts/currently-playing.sh
@@ -5,10 +5,14 @@ album=$(playerctl metadata album 2> /dev/null | cut -c 1-20)
 artist=$(playerctl metadata artist 2> /dev/null | cut -c 1-20)
 title=$(playerctl metadata title 2> /dev/null | cut -c 1-20)
 
-if [ "$player_status" = "Playing" ]; then
+if [ "$player_status" = "Playing" ] && [ "$album" != "" ]; then
   echo "  [$album] $artist - $title"
-elif [ "$player_status" = "Paused" ]; then
+elif [ "$player_status" = "Paused" ] && [ "$album" != "" ]; then
   echo "  [$album] $artist - $title"
+elif [ "$player_status" = "Playing" ]; then
+  echo "  $artist - $title"
+elif [ "$player_status" = "Paused" ]; then
+  echo "  $artist - $title"
 else
   echo ""
 fi
