5a8fcc6bda06e5667a047f8ef8ea93740b77ffdb
Nathaniel Inman <nate@theoestudio.com>
2024-10-23 13:52:21 -0500

    added support for title only polybar script

--- .polybar-scripts/currently-playing.sh
+++ .polybar-scripts/currently-playing.sh
@@ -5,14 +5,18 @@ album=$(playerctl metadata album 2> /dev/null | cut -c 1-20)
 artist=$(playerctl metadata artist 2> /dev/null | cut -c 1-20)
 title=$(playerctl metadata title 2> /dev/null | cut -c 1-20)
 
-if [ "$player_status" = "Playing" ] && [ "$album" != "" ]; then
+if [ "$player_status" = "Playing" ] && [ "$album" != "" ] && [ "$artist" != "" ]; then
   echo "  [$album] $artist - $title"
-elif [ "$player_status" = "Paused" ] && [ "$album" != "" ]; then
+elif [ "$player_status" = "Paused" ] && [ "$album" != "" ] && [ "$artist" != "" ]; then
   echo "  [$album] $artist - $title"
-elif [ "$player_status" = "Playing" ]; then
+elif [ "$player_status" = "Playing" ] && [ "$artist" != "" ]; then
   echo "  $artist - $title"
-elif [ "$player_status" = "Paused" ]; then
+elif [ "$player_status" = "Paused" ] && [ "$artist" != "" ]; then
   echo "  $artist - $title"
+elif [ "$player_status" = "Playing" ]; then
+  echo "  $title"
+elif [ "$player_status" = "Paused" ]; then
+  echo "  $title"
 else
   echo ""
 fi
