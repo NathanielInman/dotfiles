af91aa78293df1de9c6bb2edf011306474f14811
Nathaniel Inman <nate@theoestudio.com>
2026-03-11 11:21:50 -0500

    fix: override statusline lsp indicator to use get_clients with bufnr filter

--- packages/nvim/.config/nvim/lua/chadrc.lua
+++ packages/nvim/.config/nvim/lua/chadrc.lua
@@ -12,5 +12,18 @@ return {
       -- keep it displayed even with 1 tab
       lazyload = false,
     },
+    statusline = {
+      modules = {
+        -- override lsp to use get_clients with bufnr filter
+        -- instead of deprecated client.attached_buffers
+        lsp = function()
+          local clients = vim.lsp.get_clients { bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0) }
+          if #clients > 0 then
+            return (vim.o.columns > 100 and "%#St_Lsp#   LSP ~ " .. clients[1].name .. " ") or "%#St_Lsp#   LSP "
+          end
+          return ""
+        end,
+      },
+    },
   },
 }
