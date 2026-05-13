d52c1c1d5240ff0469f31e359caceb0342c79b54
Nathaniel Inman <nate@theoestudio.com>
2026-05-12 19:33:43 -0500

    refactor(nvim): update auto-session keymaps and enable auto-restore/save
    Consolidate session find/load to AutoSession search, add restore (cwd)
    binding, switch toggle to AutoSession toggle, and enable auto_restore and
    auto_save in config.

--- packages/nvim/.config/nvim/lua/plugins/auto-session.lua
+++ packages/nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -6,14 +6,19 @@ return {
     keys = {
       -- Will use Telescope if installed or a vim.ui.select picker otherwise
       { '<leader>ws', '<cmd>AutoSession save<CR>', desc = 'Save session' },
-      { '<leader>wf', '<cmd>AutoSession search<CR>', desc = 'Session find' },
       { '<leader>wd', '<cmd>AutoSession delete<CR>', desc = 'Session delete' },
-      { '<leader>wl', '<cmd>SessionRestore<CR>', desc = 'Session load' },
-      { '<leader>wt', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
+      { '<leader>wf', '<cmd>AutoSession search<CR>', desc = 'Session load (pick)' },
+      { '<leader>wl', '<cmd>AutoSession search<CR>', desc = 'Session load (pick)' },
+      { '<leader>wr', '<cmd>AutoSession restore<CR>', desc = 'Session restore (cwd)' },
+      { '<leader>wt', '<cmd>AutoSession toggle<CR>', desc = 'Toggle autosave' },
     },
     config = function()
       require('auto-session').setup {
         log_level = 'error',
+        auto_restore_enabled = true,
+        auto_save_enabled = true,
+        auto_session_enable_last_session = false,
+        auto_session_use_git_branch = false,
       }
     end,
   },
